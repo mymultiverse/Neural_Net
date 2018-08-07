c94f83f75d685db0e963776a74a18de1efea0566
Nebula <yrajsm@gmail.com>
2018-08-07 11:53:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,25 +3,34 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.utils import shuffle
 from nn import NNet
-import os
-
-print(os.listdir("input"))
 import pandas as pd
 
-train = pd.read_csv("input/train.csv",sep=',')
-test  = pd.read_csv("input/test.csv",sep=',')
+# For MNIST data
+# train = pd.read_csv("input/train.csv",sep=',')
+# test  = pd.read_csv("input/test.csv",sep=',')
 
-train_x = train.values[:,1:]
-train_y = train.values[:,0]
-# Write to the log:
-print("Training set {}has".format(train_x.shape))
-print("Test set has {} columns".format(train_y.shape))
+# train_x = train.values[:,1:]
+# train_y = train.values[:,0]
 
+# IRIS DATA
+iris = datasets.load_iris()
+# shuffle for generalized training as data arrange in catagory order
+iris.data , iris.target = shuffle(iris.data,iris.target)
 
+l = 120 # data split
+train_x = iris.data[:l,:]
+train_y = iris.target[:l]
+
+test_x = iris.data[l:,:]
+test_y = iris.target[l:]
 
 X = train_x
-Y = train_y
-layer_dims=[784,15,10] # desired structure of NN with nodes in each layer
+Y = train_y # Target output
+
+
+ind = 4
+outd = 3
+layer_dims=[ind,8,6,outd] # desired structure of NN with nodes in each layer
 
 lr = 0.03 # learning rate
 iterations = 20000
